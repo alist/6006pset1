166750c1436109f259f0f13ccea5b829f6d04bc3
Alexander List <atlex2@gmail.com>
2016-02-18 18:22:05 -0500

    type up through problem 3

--- docdist.py
+++ docdist.py
@@ -57,6 +57,7 @@ def doc_dist(word_list1, word_list2):
     cosign = dot / denominator
 
     radians = math.acos(cosign)
+    print radians
     return radians
 
 ##############################################
@@ -108,7 +109,54 @@ def doc_dist_50(word_list1, word_list2):
     words from both files
     """
     #pylint: disable=W0110,W0141
-    # well you could iterate 50 times to find the highest element, since we're zipped together
-    # hmm... how should you do that
-    #vectors that count just the 50 words that appear most frequently for each file in the comparison
-    return 0
+    # 1. get the frequency of the words for each list
+    lists = [word_list1, word_list2]
+    frequencyLists = [{"emptyexample" : 0} for _ in range(len(lists))]
+    for li, alist in enumerate(lists):
+        for word in alist:
+            frequencyLists[li][word] = 1 if word not in frequencyLists[li] else \
+                1 + frequencyLists[li][word]
+
+    # 2. Sort lists of words by frequency
+    sortedFrquencyLists = [[] for _ in range(len(lists))]
+    # TODO Why do I need an alist2 here? If it's alist, it gives me "using possibly undefined loop variable"
+    alist2=None
+    for li, alist2 in enumerate(frequencyLists):
+        sortedFrquencyLists[li] = sorted(alist2, lambda left, right: \
+            cmp(left, right) if alist2[left] == alist2[right] else 1 if alist2[left] > alist2[right] else -1, reverse=True)
+
+    # 3. Truncate sorted word lists to 50 words per list
+    trimmedLists = map(lambda alist: alist[:50], sortedFrquencyLists)
+
+    # 4. Create a new dictionary to maps words to array containing the word's count for each word list
+        # 4i. Iterate through each truncated word list
+        # 4ii. Iterate through each word in truncated word list
+        # 4iii. Add frequency of word to the dictionary entry for the word, at the column for the current iterating list
+    frequencies = {"emptyexample" : [0, 0]}
+    for ai, alist in enumerate(trimmedLists):
+        for word in alist:
+            #addition array for particular word
+            addArray = [0]*(len(lists))
+            addArray[ai] = frequencyLists[ai][word] if word in frequencyLists[ai] else 0
+            frequencies[word] = addArray if word not in frequencies else \
+                map(lambda a, b: a + (b if b is not None else 0), addArray, frequencies[word])
+
+    # 5. Note that the dictionary has more than 50 elements.
+
+    # 6. Get dot-product over dictionary's frequency values for each word. The same as in my passing (b) and (a), but on reduced data.
+    magsSquared = [0.0]*len(lists)
+    dot = 0.0
+    for freq in frequencies.itervalues():
+        magsSquared = map(lambda x, y: x + pow(y, 2), magsSquared, freq)
+        dot = dot + reduce(lambda x, y: x * y, freq)
+
+    mags = map(math.sqrt, magsSquared)
+    denominator = reduce(lambda x, y: x * y, mags)
+    cosign = dot / denominator
+
+    radians = math.acos(cosign)
+    # output:
+    # 0.423977014818
+    # 0.963287391907
+    # 0.871419668472
+    return radians
